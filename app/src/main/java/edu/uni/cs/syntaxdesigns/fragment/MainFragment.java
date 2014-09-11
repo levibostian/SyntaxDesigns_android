f7ff2bfa07249b122563d915fccc06fbe6c62808
Levi Bostian <bostianl@uni.edu>
2014-09-10 20:28:44 -0500

    Moved code around from template. Make it feel more like home.

---
+++ app/src/main/java/edu/uni/cs/syntaxdesigns/fragment/MainFragment.java
@@ -0,0 +1,35 @@
+package edu.uni.cs.syntaxdesigns.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import edu.uni.cs.syntaxdesigns.R;
+import edu.uni.cs.syntaxdesigns.activity.MainActivity;
+
+public class MainFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public static MainFragment newInstance(int sectionNumber) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
