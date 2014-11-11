c094c48c838861800fbbb52a83544cf4b84b3133
Levi Bostian <levi.bostian@gmail.com>
2014-11-10 19:07:06 -0600

    Move database query interaction test stuff to the SavedRecipesFragment.

--- app/src/main/java/edu/uni/cs/syntaxdesigns/fragment/SavedRecipesFragment.java
+++ app/src/main/java/edu/uni/cs/syntaxdesigns/fragment/SavedRecipesFragment.java
@@ -5,12 +5,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import edu.uni.cs.syntaxdesigns.R;
+import edu.uni.cs.syntaxdesigns.application.SyntaxDesignsApplication;
+import edu.uni.cs.syntaxdesigns.database.cursor.IngredientsCursor;
+import edu.uni.cs.syntaxdesigns.database.cursor.RecipeCursor;
+import edu.uni.cs.syntaxdesigns.database.dao.IngredientsDao;
+import edu.uni.cs.syntaxdesigns.database.dao.RecipeDao;
 import edu.uni.cs.syntaxdesigns.fragment.filter.SavedRecipesFilterFragment;
+
+import javax.inject.Inject;
 
 public class SavedRecipesFragment extends FilteringFragment {
 
     private SavedRecipesFilterFragment mFilterFragment;
+
+    private EditText mRecipeName;
+    private EditText mIngredientName;
+    private CheckBox mHaveIngredient;
+    private CheckBox mFavoriteRecipe;
+    private Button mAddDataButton;
+    private TextView mRecipeNameRead;
+    private TextView mIngredientNameRead;
+    private TextView mHaveIngredientRead;
+    private TextView mIsFavoriteRead;
+    private Button mReadDataButton;
+    private EditText mRecipeRowId;
+
+    @Inject RecipeDao mRecipeDao;
+    @Inject IngredientsDao mIngredientsDao;
 
     public static SavedRecipesFragment newInstance() {
         SavedRecipesFragment fragment = new SavedRecipesFragment();
@@ -23,6 +50,8 @@ public class SavedRecipesFragment extends FilteringFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        SyntaxDesignsApplication.inject(this);
+
         mFilterFragment = SavedRecipesFilterFragment.newInstance();
     }
 
@@ -30,8 +59,63 @@ public class SavedRecipesFragment extends FilteringFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_saved_recipes, container, false);
 
+        mRecipeName = (EditText) rootView.findViewById(R.id.recipe_name);
+        mIngredientName = (EditText) rootView.findViewById(R.id.ingredient_name);
+        mHaveIngredient = (CheckBox) rootView.findViewById(R.id.have_ingredient);
+        mFavoriteRecipe = (CheckBox) rootView.findViewById(R.id.favorite_recipe);
+        mAddDataButton = (Button) rootView.findViewById(R.id.add_data_button);
+        mRecipeNameRead = (TextView) rootView.findViewById(R.id.recipe_name_read);
+        mIngredientNameRead = (TextView) rootView.findViewById(R.id.ingredient_name_read);
+        mHaveIngredientRead = (TextView) rootView.findViewById(R.id.have_ingredient_read);
+        mIsFavoriteRead = (TextView) rootView.findViewById(R.id.is_favorite_read);
+        mReadDataButton = (Button) rootView.findViewById(R.id.read_data_button);
+        mRecipeRowId = (EditText) rootView.findViewById(R.id.recipe_row_id);
+
+        mAddDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addDataToDatabase();
+            }
+        });
+
+        mReadDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mRecipeRowId.getText() != null) {
+                    readDataFromDatabase();
+                } else {
+                    Toast.makeText(getActivity(), "Enter recipe rowId to read", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         return rootView;
     }
+
+    private void addDataToDatabase() {
+        long recipeRowId = mRecipeDao.insertRecipe(String.valueOf(mRecipeName.getText()), "http:www.yummly.com/" + mRecipeName);
+
+        mIngredientsDao.insertIngredient(String.valueOf(mIngredientName.getText()), mHaveIngredient.isChecked(), recipeRowId);
+        mRecipeDao.favoriteRecipe(recipeRowId, mFavoriteRecipe.isChecked());
+
+        Toast.makeText(getActivity(), "Added", Toast.LENGTH_SHORT).show();
+    }
+
+    private void readDataFromDatabase() {
+        RecipeCursor recipeCursor = mRecipeDao.readRecipeByRowId(Long.valueOf(String.valueOf(mRecipeRowId.getText())));
+        recipeCursor.moveToFirst();
+        IngredientsCursor ingredientsCursor = mIngredientsDao.readIngredientsForRecipe(recipeCursor.readRowId());
+        ingredientsCursor.moveToFirst();
+
+        mRecipeNameRead.setText(recipeCursor.readName());
+        mIngredientNameRead.setText(ingredientsCursor.readName());
+        mHaveIngredientRead.setText(ingredientsCursor.isHaveIt() ? "has it" : "does not have it");
+        mIsFavoriteRead.setText(recipeCursor.isFavorite() ? "is favorite" : "not favorite");
+
+        recipeCursor.close();
+        ingredientsCursor.close();
+    }
+
 
     @Override
     public Fragment getFilterFragment() {
--- app/src/main/java/edu/uni/cs/syntaxdesigns/module/FragmentModule.java
+++ app/src/main/java/edu/uni/cs/syntaxdesigns/module/FragmentModule.java
@@ -3,8 +3,11 @@ package edu.uni.cs.syntaxdesigns.module;
 import dagger.Module;
 import edu.uni.cs.syntaxdesigns.fragment.GroceryListFragment;
 import edu.uni.cs.syntaxdesigns.fragment.NewRecipesFragment;
+import edu.uni.cs.syntaxdesigns.fragment.SavedRecipesFragment;
 
-@Module(injects = {NewRecipesFragment.class, GroceryListFragment.class}, complete = false)
+@Module(injects = {NewRecipesFragment.class,
+                   GroceryListFragment.class,
+                   SavedRecipesFragment.class}, complete = false)
 public class FragmentModule {
 
     public FragmentModule() {
